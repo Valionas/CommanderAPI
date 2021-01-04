071c4a6aa5bd6e38d61f2987a96ada2a4d8ca72d
Valentin <sdms@abv.bg>
2021-01-04 12:41:49 +0200

    Update SQLEventRepo.cs

--- CommandHelper/CommandHelper/Data/InterfacesAndRepos/SQLEventRepo.cs
+++ CommandHelper/CommandHelper/Data/InterfacesAndRepos/SQLEventRepo.cs
@@ -1,4 +1,5 @@
 ﻿using CommandHelper.Data.DataDB;
+using CommandHelper.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,51 @@ namespace CommandHelper.Data.InterfacesAndRepos
         public SQLEventRepo(CommanderContext context)
         {
             _context = context;
+        }
+        public int CreateCommand(Event evn)
+        {
+            if (evn == null)
+            {
+                throw new ArgumentNullException(nameof(evn));
+            }
+
+
+            var result = _context.Events.Where(x => x.StartLecture == evn.StartLecture && x.EndLecture == evn.EndLecture && x.TutorialId == evn.TutorialId).FirstOrDefault();
+            if (result == null)
+            {
+                _context.Events.Add(evn);
+
+                return _context.SaveChanges();
+
+            }
+            return 0;
+        }
+
+
+        public void DeleteCommand(Event evn)
+        {
+            if (evn == null)
+            {
+                throw new ArgumentNullException(nameof(evn));
+            }
+            _context.Events.Remove(evn);
+        }
+
+
+
+        public IEnumerable<Event> GetAllCommands()
+        {
+            return _context.Events.ToList();
+        }
+
+        public Event GetCommandById(int id)
+        {
+            return _context.Events.FirstOrDefault(p => p.Id == id);
+        }
+
+        public bool SaveChanges()
+        {
+            return (_context.SaveChanges() >= 0);
         }
     }
 }
